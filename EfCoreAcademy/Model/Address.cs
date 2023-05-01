4a76de8042751dea91f921d33955a7220aac876f
laurabrovkina <laurabrovkina@gmail.com>
2023-05-01 13:22:06 +1200

    Add EF code first

---
+++ EfCoreAcademy/Model/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace EfCoreAcademy.Model
+{
+    public class Address : BaseEntity
+    {
+        public string City { get; set; } = default!;
+        public string Zip { get; set; } = default!;
+        public string Street { get; set; } = default!;
+        public int HouseNubmber { get; set; }
+
+    }
+}
---
+++ EfCoreAcademy/Program.cs
@@ -0,0 +1,10 @@
+﻿//Install EF Core tools: tool install --global dotnet-ef
+//Creating migration: dotnet ef migrations ad InitialMigration
+
+using EfCoreAcademy;
+using Microsoft.EntityFrameworkCore;
+
+var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseSqlite().Options;
+var dbContext = new ApplicationDbContext(options);
+
+dbContext.Database.Migrate();
